ed345655d14419d12dcb7b0e4d88f8ac46fdc162
khaled <khalodatia@gmail.com>
2026-03-25 22:10:43 +0200

    ch08 ex01

---
+++ ch08/ex01/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Multipliable interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
+}
+
+func Double[T Multipliable](val T) T {
+	return val * T(2)
+}
+
+func main() {
+	var i int8 = 2
+	fmt.Println(Double(i))
+	var ii int64 = 89
+	fmt.Println(Double(ii))
+
+	var f float32 = 3.4
+	fmt.Println(f)
+	var ff float64 = 55.43
+	fmt.Println(Double(ff))
+}
